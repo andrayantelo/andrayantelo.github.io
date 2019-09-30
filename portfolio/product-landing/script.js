e9212ba1885fd155ec5b217532b48408633ca5cb
andrayantelo <andrayantelo@gmail.com>
2019-09-29 18:10:52 -0700

    deleted comments from script files

--- portfolio/product-landing/script.js
+++ portfolio/product-landing/script.js
@@ -8,13 +8,3 @@ function myFunction() {
     /*nav.style.height = "150px";*/
   }
 }
-
-
-// TODO
-/*make a animation or whatever where the car image drives to the right and off screen and then it's back where it started 
-
-Do I want to change img widths to be 100% instead of 100vmax?
-Replace images with free stock photos
-
-consolidate CSS
-*/
--- portfolio/technical-documentation/script.js
+++ portfolio/technical-documentation/script.js
@@ -1,5 +1,1 @@
-//TODO
-/*
-can't see scrollbar on navbar in mobile view,
-make sure page is accessible, consolidate code, check specs
-get rid of article element or move them under header element*/
+
