d372a40d67af5a30ab777be7943997313aecee48
andrayantelo <andrayantelo@gmail.com>
2019-09-29 18:12:50 -0700

    emptied source script file

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-// for later use
-//var showMessage = function (target, message, index, interval) {   
-//  if (index < message.length) {
-// document.getElementById(target).innerHTML += message[index++];
-//    setTimeout(function () { showMessage(target, message, index, interval); }, interval);
-//  }
-//}
+
