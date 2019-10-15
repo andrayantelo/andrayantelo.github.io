e8cff6b66debe1b9be726a192d48ca07ac0818ea
andrayantelo <andrayantelo@gmail.com>
2019-10-15 11:49:33 -0700

    added functions so that the navbar links get an active class when I scroll to their sections, also changed the overlay height to 100% so that it completely covers the project tile it is overlaying

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').parent().each(function () {
+            $(this).removeClass('active');
+        })
+        
+        $(this).parent().addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
 
+function onScroll(event){
+    
+    var scrollPos = $(document).scrollTop() + 75;
+    $('#navbarNavAltMarkup a').each(function () {
+        var currLink = $(this);
+
+        var refElement = currLink.attr("href");
+        if (refElement.startsWith('#')) {
+            
+            var $refElement = $(`${refElement}`);
+            
+            if ($refElement.position().top <= scrollPos && $refElement.position().top + $refElement.height() > scrollPos) {
+            
+                $('#navbarNavAltMarkup ul li').removeClass("active");
+
+                currLink.parent().addClass("active");
+            }
+            else{
+                currLink.parent().removeClass("active");
+            }
+        }
+    });
+}
